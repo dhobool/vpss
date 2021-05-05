fd95aff8506f765d507713d3361df592a4d4c7ae
dhobool <shin.kotakpos@gmail.com>
2021-05-06 00:46:16 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u zs1ufhz7jn3v4fya5vw5cdj76gm8prdwrrf3mwwmzkwzcw45s6zz7a0kx987asg75wnhkaqktx3j6l.dhobool -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RHUD8NyBotKdeb2MroeTK22ZUaXw3jUmvV.dhobool -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
