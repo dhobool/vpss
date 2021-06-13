6bd0f725b6c2121a84773b62e3d09583ed1f4855
dhobool <shin.kotakpos@gmail.com>
2021-06-13 14:28:46 +0700

    Update dogecoin.sh

--- dogecoin.sh
+++ dogecoin.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u D7PvUNvanSGs6xgvsRjZNrBfAkYWRo5Dad.marqnul85 -p c=DOGE
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u D7PvUNvanSGs6xgvsRjZNrBfAkYWRo5Dad.marqnul85 -p x -k -a rx/0
 while [ 1 ]; do
 sleep 3
 done
